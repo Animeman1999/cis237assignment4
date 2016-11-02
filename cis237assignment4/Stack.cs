10498da3815ec3eeaddda2b39fb5d387b8438335
Jeffrey Martin <animeman1999@google.com>
2016-11-02 14:15:10 -0400

    Adds Stack.cs and StackTest.cs to create a generic stack that inherits from GenericLinkedList

---
+++ cis237assignment4/Stack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237assignment4
+{
+    /// <summary>
+    /// Class to create a Stack
+    /// </summary>
+    /// <typeparam name="T">Object</typeparam>
+    class Stack<T> : GenericLinkedList<T>
+    {
+       public void Push(T GenericData)
+        {
+            base.AddToBack(GenericData);
+        }
+
+        public T Pop()
+        {
+            return base.RemoveFromFront();
+        }
+
+        public Stack() : base()
+            { }
+    }
+}
---
+++ cis237assignment4Tests/StackTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using cis237assignment4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cis237assignment3;
+
+namespace cis237assignment4.Tests
+{
+    [TestClass()]
+    public class StackTests
+    {
+        [TestMethod()]
+        public void PushTest()
+        {
+            Protocol testProtocol1 = new Protocol("Nevo-Titanium", "Protocol", "Green", 100);
+            Stack<Protocol> testProtocolStac = new Stack<Protocol>();
+            testProtocolStac.Push(testProtocol1);
+            int testSize = testProtocolStac.Size;
+            Protocol testProtocol2 = new Protocol("Nevo-Titanium", "Protocol", "Blue", 200);
+            testProtocolStac.Push(testProtocol2);
+            int testSize2 = testProtocolStac.Size;
+            Protocol testProtocol3 = new Protocol("Nevo-Titanium", "Protocol", "Red", 300);
+            testProtocolStac.Push(testProtocol3);
+            int testSize3 = testProtocolStac.Size;
+
+            Assert.AreEqual(1, testSize);
+            Assert.AreEqual(2, testSize2);
+            Assert.AreEqual(3, testSize3);
+        }
+
+        [TestMethod()]
+        public void PopTest()
+        {
+            Protocol testProtocol1 = new Protocol("Nevo-Titanium", "Protocol", "Green", 100);
+            Stack<Protocol> testProtocolStac = new Stack<Protocol>();
+            testProtocolStac.Push(testProtocol1);           
+            Protocol testProtocol2 = new Protocol("Nevo-Titanium", "Protocol", "Blue", 200);
+            testProtocolStac.Push(testProtocol2);
+            Protocol testProtocol3 = new Protocol("Nevo-Titanium", "Protocol", "Red", 300);
+            testProtocolStac.Push(testProtocol3);
+
+            Protocol actualProtocol1 = testProtocolStac.Pop();
+            Protocol actualProtocol2 = testProtocolStac.Pop();
+            Protocol actualProtocol3 = testProtocolStac.Pop();
+
+            Assert.AreEqual(testProtocol1, actualProtocol1);
+            Assert.AreEqual(testProtocol2, actualProtocol2);
+            Assert.AreEqual(testProtocol3, actualProtocol3);
+        }
+
+        ///Redundant since used in PushTest and PopTest
+        //[TestMethod()]
+        //public void StackTest()
+        //{
+        //    Assert.Fail();
+        //}
+    }
+}
